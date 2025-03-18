0ad0b21ea3269df2d66884e35082684e6999c0dd
badruzbby <rohmancrafter846@gmail.com>
2025-03-18 11:18:04 -0500

    Memperbaiki urutan impor di user.rs dengan memindahkan import bcrypt ke posisi yang tepat untuk meningkatkan keterbacaan kode.

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
-use bcrypt::{DEFAULT_COST, hash};
 #[cfg(not(debug_assertions))]
 use bcrypt::verify;
+use bcrypt::{DEFAULT_COST, hash};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, postgres::PgPool};
