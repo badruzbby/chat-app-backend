b16933c4e43f544701c0477df8d355037ef48256
badruzbby <rohmancrafter846@gmail.com>
2025-03-18 11:16:40 -0500

    Fix: Perbaikan import kondisional untuk verify di user.rs

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
-use bcrypt::{DEFAULT_COST, hash, verify};
+use bcrypt::{DEFAULT_COST, hash};
+#[cfg(not(debug_assertions))]
+use bcrypt::verify;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, postgres::PgPool};
